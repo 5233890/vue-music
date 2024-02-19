3e3f208ee610b07b6c5959b50121e10233909577
luyudong <897789206@qq.com>
2024-02-19 22:35:50 +0800

    安装stylus stylus-loader插件 && 添加styl和字体相关文件 && 相对路径设置

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+
+import 'common/stylus/index.styl'
+/* eslint-disable no-unused-vars */
 
 Vue.config.productionTip = false
 
