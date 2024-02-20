65f576d86f073ce05bee8db46cf6e882495c1dab
luyudong <897789206@qq.com>
2024-02-20 21:53:47 +0800

    安装fastclick插件 && 添加m-header组件

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastclick from 'fastclick'
 
 import 'common/stylus/index.styl'
 /* eslint-disable no-unused-vars */
-
+fastclick.attach(document.body)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
