6d10e2a736c828876091ea8c1a61ddb3d03eda0f
luyudong <897789206@qq.com>
2024-02-29 20:14:55 +0800

    引入jsonp，并用jsonp请求推荐接口

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,9 +1,23 @@
 <template>
-  <div>recommand</div>
+  <div>recommend</div>
 </template>
 
 <script>
+  import {getCommend} from 'api/recommend.js'
+  import {ERR_OK} from 'api/config'
   export default {
+    created () {
+      this._getRecommend()
+    },
+    methods: {
+      _getRecommend () {
+        getCommend().then(res => {
+          if (res.code === ERR_OK) {
+            console.log('res', res.data.slider)
+          }
+        })
+      }
+    }
   }
 </script>
 
